17f4d156d6ed807a53cbe283254126e1106074f8
Abhishek Dangi <72682273+Abhi-AD@users.noreply.github.com>
2024-02-08 23:32:07 +0545

    custom error and input form

--- hello copy.py
+++ hello copy.py
@@ -1,19 +1,7 @@
-# import list
 from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
 
 # create a flask instance
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "secretkey"
-
-
-# Create a Form Class
-class NameForm(FlaskForm):
-    name = StringField("What is your name?", validators=[DataRequired()])
-    submit = SubmitField("Submit")
 
 
 # create a route decorator
@@ -47,19 +35,8 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 
+
 # Internal Server Erro
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
-
-
-# Create Name Page
-@app.route("/name", methods=["GET", "POST"])
-def name():
-    name = None
-    form = NameForm()
-    # Validate Form
-    if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ""
-    return render_template("nameinput.html", name=name, form=form)
